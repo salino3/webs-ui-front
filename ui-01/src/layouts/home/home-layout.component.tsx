6a544a972fa4fc32eacef00e2a7cb8cf39136aba
Salino3 <flaviobcn012@gmail.com>
2026-03-05 16:05:37 +0200

    save changes

--- ui-01/src/layouts/home/home-layout.component.tsx
+++ ui-01/src/layouts/home/home-layout.component.tsx
@@ -1,6 +1,12 @@
 import type React from "react";
+import { Header } from "../../common-app";
 import "./home-layout.styles.scss";
 
 export const HomeLayout: React.FC = () => {
-  return <div className="rootHomeLayout">rootHomeLayout</div>;
+  return (
+    <div className="rootHomeLayout">
+      <Header />
+      <h1>rootHomeLayout</h1>
+    </div>
+  );
 };
