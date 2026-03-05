d42dcf28a71d0b0b97c79044fcb71d03010b4ad3
Salino3 <flaviobcn012@gmail.com>
2026-03-05 18:01:24 +0200

    fix transaction button side bar

--- ui-01/src/common-app/aside/aside.component.tsx
+++ ui-01/src/common-app/aside/aside.component.tsx
@@ -2,13 +2,22 @@ import React, { useState } from "react";
 import "./aside.styles.scss";
 
 export const Aside: React.FC = () => {
-  const [widthSideBar, setWidthSideBar] = useState<string>("250px");
+  const [isExpanded, setIsExpanded] = useState<string>("250px");
   return (
-    <div className="rootAside" style={{ flex: `0 1 ${widthSideBar}` }}>
-      <button onClick={() => setWidthSideBar(!!widthSideBar ? "" : "250px")}>
-        {!!widthSideBar ? "<<" : ">>"}
-      </button>
-      <h4 style={{ fontSize: `${!!widthSideBar ? "22px" : "12px"}` }}>Aside</h4>
+    <div className="rootAside" style={{ flex: `0 1 ${isExpanded}` }}>
+      <div className="boxBtnSideBar">
+        <div
+          className="spacer"
+          style={{ width: isExpanded ? "180px" : "0px" }}
+        />
+        <button
+          className="btnSideBar"
+          onClick={() => setIsExpanded(!!isExpanded ? "" : "250px")}
+        >
+          {!!isExpanded ? "<<" : ">>"}
+        </button>
+      </div>
+      <h4 style={{ fontSize: `${!!isExpanded ? "22px" : "12px"}` }}>Aside</h4>
     </div>
   );
 };
