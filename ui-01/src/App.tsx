71f1e619a9f32366b63ad2e8e74aac14a7e17fb7
Salino3 <flaviobcn012@gmail.com>
2026-03-05 15:58:56 +0200

    create header

--- ui-01/src/App.tsx
+++ ui-01/src/App.tsx
@@ -1,9 +1,11 @@
 import { HomeLayout } from "./layouts";
+import { Header } from "./common-app";
 import "./App.scss";
 
 function App() {
   return (
     <div className="rootApp">
+      <Header />
       <HomeLayout />
     </div>
   );
