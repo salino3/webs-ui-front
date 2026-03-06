b8fda4fee842abf7d158b39a637c11b761860ad4
Salino3 <flaviobcn012@gmail.com>
2026-03-06 11:14:08 +0200

    add content to header

--- ui-01/src/common-app/header/header.component.tsx
+++ ui-01/src/common-app/header/header.component.tsx
@@ -4,7 +4,18 @@ import "./header.styles.scss";
 export const Header: React.FC = () => {
   return (
     <header className="rootHeader">
-      <h2>Header Page</h2>
+      <div className="boxLeft">
+        <div className="boxIcon"></div>
+        <h2>Title Header Page</h2>
+      </div>
+      <div className="boxRight">
+        <nav>
+          <a href="">link 1</a>
+          <a href="">link 2</a>
+          <a href="">link 3</a>
+          <a href="">link 4</a>
+        </nav>
+      </div>
     </header>
   );
 };
