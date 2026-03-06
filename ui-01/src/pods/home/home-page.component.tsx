67d413fd3b0529ebf52d012d13eb067257419870
Salino3 <flaviobcn012@gmail.com>
2026-03-06 11:14:54 +0200

    save changes

--- ui-01/src/pods/home/home-page.component.tsx
+++ ui-01/src/pods/home/home-page.component.tsx
@@ -5,6 +5,75 @@ export const HomePage: React.FC = () => {
   return (
     <div className="rootHomePage">
       <h1>Root Home Layout</h1>
+      <li>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+        repellat, eius qui tempore impedit molestiae nobis vel praesentium omnis
+        perferendis voluptate accusamus dolorum esse ex minima repudiandae est
+        magnam quas? Reprehenderit sit non earum temporibus ducimus nemo ea
+        laborum, voluptate corrupti impedit excepturi inventore vel quos sint
+        cumque vero dolorem porro? Reprehenderit inventore, nisi dolor
+        consequatur eos labore esse explicabo. Modi consequatur eum ducimus
+        deleniti. Doloribus fugit facere a ex eius ab corporis, nam, ratione
+        similique cupiditate molestiae quod quidem quaerat quis. Aliquam veniam
+        quidem aut ut eius commodi animi? Quam fugit quaerat error ipsa quia
+        deserunt, sint vero doloremque esse, pariatur molestiae eligendi
+        aliquid? Accusantium, dolorum minima exercitationem impedit repudiandae
+        doloribus officiis repellendus modi numquam laudantium alias assumenda
+        odio? Similique illo nesciunt corrupti, doloribus nam enim cum
+        dignissimos consectetur delectus incidunt rem aperiam laudantium
+        officiis distinctio natus libero! Sed fuga eos id! Quia corporis animi,
+        unde quas voluptate ipsam! Ad consectetur iure sint veritatis id
+        repellendus minima? Iste dolorem animi veritatis inventore architecto,
+        vel nam iusto sed odit magnam consectetur. Debitis nesciunt sequi minus
+        nostrum nisi eligendi consectetur reiciendis? Tenetur debitis aspernatur
+        suscipit recusandae animi voluptatum ea voluptas officia reiciendis,
+        ducimus, sapiente magnam labore laboriosam molestiae reprehenderit ex
+        quae quam sequi. Enim incidunt adipisci, nemo odit ut minus dolor.
+        Beatae reiciendis nemo labore culpa. Dolor perferendis porro tempora
+        commodi, ipsam saepe atque molestias odit consequuntur. Perspiciatis
+        natus asperiores ipsum. Debitis, maiores placeat quos ducimus officiis
+        voluptates molestias vitae quasi. Officiis perspiciatis eligendi itaque
+        suscipit asperiores culpa facere sequi esse tempora reiciendis minima
+        fugit, iste quibusdam obcaecati animi! Iusto ea eaque perspiciatis
+        error, pariatur voluptatum alias corrupti nisi incidunt commodi. Totam
+        eos quisquam quasi ullam illum ipsa numquam perferendis unde accusamus
+        sequi ipsam veniam id harum provident voluptates tempore, delectus amet
+        facilis dolorem velit quos, possimus nisi! Minima, error doloribus. Quod
+        unde consequuntur suscipit in doloremque expedita amet ullam est
+        molestias assumenda pariatur possimus quo enim adipisci sunt aliquam a,
+        ab, illo nihil nobis dolores odio, molestiae necessitatibus? Qui, illo.
+        Error temporibus ducimus laborum quidem praesentium inventore!
+        Repudiandae cupiditate consequuntur doloremque eligendi? Eligendi sequi
+        adipisci amet blanditiis, fugit quam minima placeat, incidunt doloremque
+        perspiciatis eius inventore voluptatibus ipsum consectetur fugiat.
+        Accusantium ab iste rem perferendis quis mollitia corrupti ipsam esse
+        nobis! Maxime assumenda iusto nulla ex ratione eos! Sequi nobis numquam
+        nam quos dolorum nulla consectetur aperiam in eius enim. Vel asperiores
+        laudantium sapiente, at explicabo corporis mollitia. Illum, accusamus
+        reprehenderit. Rem, nemo molestias? Obcaecati eius mollitia
+        reprehenderit provident consectetur unde maxime. Assumenda harum, quis
+        ullam unde doloremque aliquam dignissimos! Obcaecati quam laborum
+        consequuntur suscipit tempore repudiandae odio omnis animi inventore
+        officia deleniti, quisquam libero dicta quibusdam incidunt minus tempora
+        amet quis corporis tenetur modi maxime, in ullam! Minima, quisquam?
+        Tenetur, minus, ab dolorem harum velit fugit dolores voluptate
+        voluptatem cum doloremque quod consectetur. Ipsum iste beatae incidunt
+        numquam excepturi vero, quisquam quia architecto quo necessitatibus, hic
+        officia labore ab. Aliquam debitis sequi assumenda perspiciatis
+        voluptates. Molestiae ipsum asperiores at alias temporibus, sed modi
+        excepturi ipsam perspiciatis libero. Quis optio animi incidunt maxime
+        nostrum eos quasi vero ducimus quibusdam iusto. Labore fuga omnis
+        praesentium ullam iste autem voluptas quae corrupti. Praesentium, quae!
+        Culpa aut perferendis nobis nisi quam quis, maiores dolorem
+        reprehenderit ipsam, impedit molestiae laborum autem at? Voluptas,
+        culpa! Alias sapiente, deleniti laborum laudantium earum, corrupti
+        reiciendis dolorem eum optio dolore tempora enim soluta, cumque
+        praesentium in voluptates quo sunt neque laboriosam quas minus nemo?
+        Laboriosam ratione dolores asperiores! Totam suscipit eius quo! Nemo
+        porro rerum amet ipsam minus! Esse reprehenderit quia, qui est delectus
+        neque, aliquam optio error maxime mollitia quibusdam accusantium illum
+        voluptatibus nostrum! Voluptatem, placeat at.
+      </li>
     </div>
   );
 };
