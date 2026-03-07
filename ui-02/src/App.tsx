807b07adbacf5c1eb367895de1db68d3788749a8
Salino3 <flaviobcn012@gmail.com>
2026-03-07 10:49:54 +0200

    create second ui repository

---
+++ ui-02/src/App.tsx
@@ -0,0 +1,8 @@
+import { HomeLayout } from "./layouts";
+import "./App.scss";
+
+function App() {
+  return <HomeLayout />;
+}
+
+export default App;
---
+++ ui-02/src/common-app/header/header.component.tsx
@@ -0,0 +1,10 @@
+import type React from "react";
+import "./header.styles.scss";
+
+export const Header: React.FC = () => {
+  return (
+    <div className="rootHeader">
+      <h2>Header Page</h2>
+    </div>
+  );
+};
