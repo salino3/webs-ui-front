e997c872e8bf35c7a81a5b1a4c03e8509098460b
Salino3 <flaviobcn012@gmail.com>
2026-03-08 11:07:18 +0200

    fix styles web

--- ui-02/src/layouts/home/home-layout.component.tsx
+++ ui-02/src/layouts/home/home-layout.component.tsx
@@ -1,8 +1,14 @@
 import type React from "react";
+import { Header } from "../../common-app";
 import "./home-layout.styles.scss";
 
 export const HomeLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  return <div className="rootHomeLayout">{children}</div>;
+  return (
+    <div className="rootHomeLayout">
+      <Header />
+      {children}
+    </div>
+  );
 };
