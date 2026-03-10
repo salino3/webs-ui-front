bb00a30fb994076c11668010c4c82673d9ea1214
Salino3 <flaviobcn012@gmail.com>
2026-03-10 09:39:50 +0200

    add body home

---
+++ ui-02/src/pods/home/components/body-home/body-home.component.tsx
@@ -0,0 +1,6 @@
+import type React from "react";
+import "./body-home.styles.scss";
+
+export const BodyHome: React.FC = () => {
+  return <div className="rootBodyHome">rootBodyHome</div>;
+};
--- ui-02/src/pods/home/home-page.component.tsx
+++ ui-02/src/pods/home/home-page.component.tsx
@@ -1,10 +1,12 @@
 import type React from "react";
+import { BodyHome } from "./components";
 import "./home-page.styles.scss";
 
 const HomePage: React.FC = () => {
   return (
     <div className="rootHomepage">
       <h1>Home Page</h1>
+      <BodyHome />
     </div>
   );
 };
