2db3e0c373ae5a61a0d6c47e76bf6e91f03900b5
Salino3 <flaviobcn012@gmail.com>
2026-03-10 10:07:14 +0200

    save changes

--- ui-02/src/pods/home/components/body-home/body-home.component.tsx
+++ ui-02/src/pods/home/components/body-home/body-home.component.tsx
@@ -2,5 +2,32 @@ import type React from "react";
 import "./body-home.styles.scss";
 
 export const BodyHome: React.FC = () => {
-  return <div className="rootBodyHome">rootBodyHome</div>;
+  return (
+    <div className="rootBodyHome">
+      <div className="firstBox">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+          atque velit dolor deleniti, nihil dolorem? At eaque labore modi culpa
+          est? Architecto deleniti consequatur labore quod cum repudiandae rem
+          delectus. Harum voluptate vel ut in quam, illo dolor error, eaque
+          perspiciatis deleniti vero dolores, doloremque similique iure autem.
+          Quaerat rerum esse maiores eos vitae aliquam quisquam odit placeat
+          eius voluptas? Tempora animi neque autem, molestias repellat, incidunt
+          at dolor ducimus rem quisquam facere? Odit doloremque id veritatis
+          temporibus in! Aperiam nostrum sequi aspernatur, cumque porro
+          provident odit expedita necessitatibus est? Blanditiis accusamus
+          adipisci voluptates qui quae, voluptate tempore odio repudiandae rerum
+          iusto, tenetur soluta modi vel, velit praesentium id nihil est sunt?
+          Quia dignissimos omnis quibusdam necessitatibus nemo facere quod!
+          Consequuntur consectetur aut corrupti. Reiciendis quisquam ut veniam
+          dolor perferendis libero beatae eveniet officiis, voluptatum, nihil
+          dolorem provident cumque consequuntur, mollitia cupiditate totam
+          dolore? Officiis sit minus natus id non!
+        </p>
+        <div className="boxImage">
+          <img src="../../../../../public/test_image.png" alt="Test Image" />
+        </div>
+      </div>
+    </div>
+  );
 };
